9ef27f67383061ec0c7675cff1b6b88f9de0e5fc
Daktum <bf@voxlav.de>
2026-01-07 12:11:32 +0100

    Encrypting and Decrypting

--- src/Caesar.java
+++ src/Caesar.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Ceaser {
+public class Caesar {
 
 
     public static String encrypt(String word, int key) {
