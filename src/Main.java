f23663d22ff9da20a1dd4fc975a9b10a2c3925ce
Daktum <bf@voxlav.de>
2026-02-04 11:53:43 +0100

    Bug fixes!

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-
-
+        String vegeWord = "Zielperson Hat Heute MORGEN IM MCDONALDS DIE HERRENTOILETTE BENUTZT";
+        String vegeKey = "BIGMAC";
+        System.out.println(Vigenere.encrypt(vegeWord, vegeKey));
+        System.out.println(Vigenere.decrypt("Tvjpme Qollx tqexh iyx Xizjg Tnxh!", "INFO"));
     }
-
-
 }
