8b08c5500dc163c728fad566efac02171aec3dae
Daktum <bf@voxlav.de>
2026-02-09 19:39:36 +0100

    Clear out Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        String vegeWord = "Zielperson Hat Heute MORGEN IM MCDONALDS DIE HERRENTOILETTE BENUTZT";
-        String vegeKey = "BIGMAC";
-        System.out.println(Vigenere.encrypt(vegeWord, vegeKey));
-        System.out.println(Vigenere.decrypt("Tvjpme Qollx tqexh iyx Xizjg Tnxh!", "INFO"));
-        System.out.println("test");
+
     }
 }
