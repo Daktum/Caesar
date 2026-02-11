6bbca509bd2d2b16a4f12a72af12b6713f6ba887
Daktum <bf@voxlav.de>
2026-02-11 09:11:24 +0100

    First itaration of finding Duplicates

--- src/Kasiski.java
+++ src/Kasiski.java
@@ -1,18 +1,37 @@
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
 
 public class Kasiski {
 
 
 
-    public static ArrayList<Integer> findDuplicates(String word, String subString){
+    public static void findDuplicates(String word){
+
+        HashMap<String, ArrayList<Integer>> sub = new HashMap<>();
+
+        String subString = "";
+
         ArrayList<Integer> place = new ArrayList<>();
 
-        for (int i = 0; i <= word.length(); i++){
-            if(word.startsWith(subString, i)){
-                place.add(i);
+        for (int getSub = 0; getSub <= word.length()-2; getSub++){
+
+            subString = word.substring(getSub, getSub+3);
+
+            if(!sub.containsKey(subString)){
+                for (int i = 0; i <= word.length(); i++){
+                    if(word.startsWith(subString, i)){
+                        place.add(i);
+                    }
+                }
             }
+            sub.put(subString, place);
+            place.clear();
         }
-        return place;
+
+
+
+
     }
 
     public static void Keylength(ArrayList<Integer> place){
